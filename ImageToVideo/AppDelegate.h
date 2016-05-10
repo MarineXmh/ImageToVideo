7f22a31280d17c8f7472c9d5db984e611a84ef7f
徐梦华 <15957299@qq.com>
2016-05-10 17:37:09 +0800

    Initial Commit

---
+++ ImageToVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageToVideo
+//
+//  Created by Xu Menghua on 16/5/10.
+//  Copyright © 2016年 Xu Menghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageToVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageToVideo
+//
+//  Created by Xu Menghua on 16/5/10.
+//  Copyright © 2016年 Xu Menghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
